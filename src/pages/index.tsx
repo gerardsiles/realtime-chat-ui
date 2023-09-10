6584af1f129224ca1a748ae7d12dcc2698fdd4e1
Gerard Siles <gerardsiles@gmail.com>
2023-09-10 22:27:55 +0000

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,107 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { useEffect, useRef, useState } from 'react';
+import { io, type Socket } from 'socket.io-client';
 
-const inter = Inter({ subsets: ['latin'] })
+type Message = {
+	message: string;
+	id: string;
+	createdAt: string;
+	port: string;
+};
+
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'ws://127.0.0.1';
+const CONNECTION_COUNT_UPDATED_CHANNEL = 'chat:connection-count-updated';
+const NEW_MESSAGE_CHANNEL = 'chat:new-message';
+
+function useSocket() {
+	const [socket, setSocket] = useState<Socket | null>(null);
+
+	useEffect(() => {
+		const socketIo = io(SOCKET_URL, {
+			reconnection: true,
+			upgrade: true,
+			transports: ['websocket', 'polling'],
+		});
+
+		setSocket(socketIo);
+
+		return function () {
+			socketIo.disconnect();
+		};
+	}, []);
+
+	return socket;
+}
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const messageListRef = useRef<HTMLOListElement | null>(null);
+	const socket = useSocket();
+	const [connectionCount, setConnectionCount] = useState<number>(0);
+	const [newMessage, setNewMessage] = useState<string>('');
+	const [messages, setMessages] = useState<Message[]>([]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		socket?.on('connect', () => {
+			console.log('connected');
+		});
+		socket?.on(NEW_MESSAGE_CHANNEL, (message: Message) => {
+			setMessages(prevMessages => [...prevMessages, message]);
+			setTimeout(() => {
+				scrollOnMessage();
+			}, 0);
+		});
+		socket?.on(
+			CONNECTION_COUNT_UPDATED_CHANNEL,
+			({ count }: { count: number }) => {
+				setConnectionCount(count);
+			}
+		);
+	}, [socket]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const scrollOnMessage = () => {
+		if (messageListRef.current) {
+			messageListRef.current.scrollTop =
+				messageListRef.current.scrollHeight + 1000;
+		}
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setNewMessage('');
+		socket?.emit(NEW_MESSAGE_CHANNEL, { message: newMessage });
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className='flex flex-col p-4 w-full max-w-3xl m-auto'>
+			<h1 className='text-4xl font-bold text-center mb-4'>
+				Chat ({connectionCount})
+			</h1>
+			<ol
+				className='flex-1 overflow-y-scroll overflow-x-hidden mb-4'
+				ref={messageListRef}
+			>
+				{messages.map(message => (
+					<li
+						key={message.id}
+						className='flex flex-col bg-gray-100 rounded-lg p-4 my-2 break-all mx-1'
+					>
+						<p className='text-gray-500 text-sm mb-2'>{message.createdAt}</p>
+						<p className='text-gray-500 text-sm mb-2'>{message.port}</p>
+						{message.message}
+					</li>
+				))}
+			</ol>
+			<form onSubmit={handleSubmit} className='flex items-center'>
+				<Textarea
+					className='rounded-lg mr-4'
+					placeholder='Type your message here...'
+					value={newMessage}
+					onChange={e => setNewMessage(e.target.value)}
+					maxLength={255}
+				/>
+				<Button className='h-full'>Send Message</Button>
+			</form>
+		</main>
+	);
 }
